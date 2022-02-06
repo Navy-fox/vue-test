a699970319ee4e30e223bef9317a9bf372063dc9
navyFox <lina.666@list.ru>
2022-02-06 14:29:01 +0300

    Добавила плагин v-mask для формы ввода телефона

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import main from '../components/main'
+import VueMask from 'v-mask'
 
+Vue.use(VueMask)
 Vue.use(Router)
 
 export default new Router({
